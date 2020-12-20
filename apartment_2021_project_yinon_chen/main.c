6f5c2261e74642bae63936a0d914175de84ed6fd
Yinon <inon178@gmail.com>
2020-12-20 12:50:15 +0200

    init c file

---
+++ apartment_2021_project_yinon_chen/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  apartment_2021_project
+//
+//  Created by Yinon Horev on 20/12/2020.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
